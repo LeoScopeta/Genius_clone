dc1d5d013567f4d37007084901c451995300c6d7
LeoScopeta <65691356+LeoScopeta@users.noreply.github.com>
2022-01-17 17:48:34 -0300

    Color element creator function addded

--- script.js
+++ script.js
@@ -47,3 +47,14 @@ let click = (color) => {
         checkOrder();
     },250);
 }
+let createColorElement = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
